c13b12f4c0c1baf26e3d1372f5ed929f7b0b6d78
Anton Kapustin <quech@yandex.ru>
2020-01-29 19:50:02 +0300

    updated

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ install:
 	npm install
 start:
 	npx node bin/brain-games.js
+publish:
+	npm publish --dry-run
+make lint:
+	npx eslint .
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const askForName = () => {
-    var userName = readlineSync.question('May I have your name? ');
+    let userName = readlineSync.question('May I have your name? ');
     return console.log(`Hello, ${userName}!`);
 };

57a6e0fb2a75a7ed8ce19e0bc3d3df77e8642e38
Anton Kapustin <quech@yandex.ru>
2020-02-02 17:04:24 +0300

    created new game

---
+++ src/games/primegame.js
@@ -0,0 +1,17 @@
+export default () => {
+  const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const randNumber = () => Math.ceil(Math.random() * 100);
+  const getCorrectAnswer = (num) => {
+    const arrOfDivisors = [];
+    for (let i = 2; i <= num; i += 1) {
+      if (num % i === 0) {
+        arrOfDivisors.push(i);
+      }
+    }
+    return arrOfDivisors.length > 1 ? 'no' : 'yes';
+  };
+  const result = [rules, randNumber, getCorrectAnswer];
+  return result;
+};
+
+  

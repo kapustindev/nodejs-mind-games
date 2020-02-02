166887903e4571bb2986419cebb0a972a24f5501
Anton Kapustin <quech@yandex.ru>
2020-02-02 17:33:10 +0300

    updated

--- src/games/nodgame.js
+++ src/games/nodgame.js
@@ -1,24 +1,23 @@
 export default () => {
-    const rules = 'Find the greatest common divisor of given numbers.';
-    const randNumber = () => Math.ceil(Math.random() * 100);
-    const getRandNumbers = () => `${randNumber()} ${randNumber()}`;
-    const getNod = (str) => {
-        const numArray = str.split(' ');
-        const num1 = Number(numArray[0]);
-        const num2 = Number(numArray[1]);
-        let stepCount = 0;
-        let maxDiv = 0;
-        num1 > num2 ? stepCount = num2 : stepCount = num1;
-        
-        for (let i = 1; i <= stepCount; i += 1) {
-            if (num1 % i === 0 && num2 % i === 0) {
-                maxDiv = i;
-            }
-        }
+  const rules = 'Find the greatest common divisor of given numbers.';
+  const randNumber = () => Math.ceil(Math.random() * 100);
+  const getRandNumbers = () => `${randNumber()} ${randNumber()}`;
+  const getNod = (str) => {
+    const numArray = str.split(' ');
+    const num1 = Number(numArray[0]);
+    const num2 = Number(numArray[1]);
+    let stepCount = 0;
+    let maxDiv = 0;
+    num1 > num2 ? stepCount = num2 : stepCount = num1;
 
-        return maxDiv.toString();
-    };
-    const result = [rules, getRandNumbers, getNod];
-    return result;
+    for (let i = 1; i <= stepCount; i += 1) {
+      if (num1 % i === 0 && num2 % i === 0) {
+        maxDiv = i;
+      }
+    }
+
+    return maxDiv.toString();
   };
-  
+  const result = [rules, getRandNumbers, getNod];
+  return result;
+};
--- src/games/progressgame.js
+++ src/games/progressgame.js
@@ -1,36 +1,35 @@
 export default () => {
-    const rules = 'What number is missing in the progression?';
-    const randNumber = (num) => Math.ceil(Math.random() * num);
-    const getQuestion = () => {
-        const beginOfProgression = randNumber(25);
-        const stepOfProgression = randNumber(3);
-        const hiddenIndex = Math.floor(Math.random() * 10);
-        const resultArray = [];
-        for (let i = beginOfProgression; i < beginOfProgression + (10 * stepOfProgression); i += stepOfProgression) {
-            if (i !== beginOfProgression + (hiddenIndex * stepOfProgression)) {
-                resultArray.push(i);
-            } else {
-                resultArray.push('..');
-            }
-        }
-        return resultArray.join(' ');
-    };
-    const getCorrectAnswer = (str) => {
-        const arr = str.split(' ');
-        const hiddenIndex = arr.indexOf('..');
-        let correctAnswer = 0;
-        let step = 0;
-        if (arr[hiddenIndex - 2] !== undefined) {
-            step = Number(arr[hiddenIndex - 1]) - Number(arr[hiddenIndex - 2]);
-            correctAnswer = Number(arr[hiddenIndex - 1]) + step;
-        } else {
-            step = Number(arr[hiddenIndex + 2]) - Number(arr[hiddenIndex +1]);
-            correctAnswer = Number(arr[hiddenIndex + 1]) - step;
-        }
-        return correctAnswer.toString();
-    };
-    
-    const result = [rules, getQuestion, getCorrectAnswer];
-    return result;
+  const rules = 'What number is missing in the progression?';
+  const randNumber = (num) => Math.ceil(Math.random() * num);
+  const getQuestion = () => {
+    const beginOfProgr = randNumber(25);
+    const stepOfProgr = randNumber(3);
+    const hiddenIndex = Math.floor(Math.random() * 10);
+    const resultArray = [];
+    for (let i = beginOfProgr; i < beginOfProgr + (10 * stepOfProgr); i += stepOfProgr) {
+      if (i !== beginOfProgr + (hiddenIndex * stepOfProgr)) {
+        resultArray.push(i);
+      } else {
+        resultArray.push('..');
+      }
+    }
+    return resultArray.join(' ');
   };
-  
+  const getCorrectAnswer = (str) => {
+    const arr = str.split(' ');
+    const hiddenIndex = arr.indexOf('..');
+    let correctAnswer = 0;
+    let step = 0;
+    if (arr[hiddenIndex - 2] !== undefined) {
+      step = Number(arr[hiddenIndex - 1]) - Number(arr[hiddenIndex - 2]);
+      correctAnswer = Number(arr[hiddenIndex - 1]) + step;
+    } else {
+      step = Number(arr[hiddenIndex + 2]) - Number(arr[hiddenIndex + 1]);
+      correctAnswer = Number(arr[hiddenIndex + 1]) - step;
+    }
+    return correctAnswer.toString();
+  };
+
+  const result = [rules, getQuestion, getCorrectAnswer];
+  return result;
+};
